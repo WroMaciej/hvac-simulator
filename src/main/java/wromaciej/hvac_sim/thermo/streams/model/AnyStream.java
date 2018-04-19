77e50cfc52d826e6fc367ff271227560cef3e260
Maciej Wróblewski <wro.maciej@gmail.com>
2018-04-19 14:44:22 +0200

    Bond test

--- src/main/java/wromaciej/hvac_sim/thermo/streams/model/AnyStream.java
+++ src/main/java/wromaciej/hvac_sim/thermo/streams/model/AnyStream.java
@@ -8,23 +8,23 @@ import wromaciej.hvac_sim.thermo.devices.model.basic.bonds.InletBond;
 import wromaciej.hvac_sim.thermo.devices.model.basic.bonds.OutletBond;
 
 public abstract class AnyStream extends Item {
-    private  InletBond<? extends AnyStream, Device> inletBond;
-    private  OutletBond<? extends AnyStream, Device> outletBond;
+    private  InletBond<? extends AnyStream, ? extends Device> inletBond;
+    private  OutletBond<? extends AnyStream, ? extends Device> outletBond;
 
     public AnyStream(int id, IdGenerator idGenerator) {
         super(id, idGenerator);
     }
 
-    protected void setBonds(InletBond<? extends AnyStream, Device> inletBond, OutletBond<? extends AnyStream, Device> outletBond){
+    protected void setBonds(InletBond<? extends AnyStream, ? extends Device> inletBond, OutletBond<? extends AnyStream, ? extends Device> outletBond){
         this.inletBond = inletBond;
         this.outletBond = outletBond;
     }
 
-    public InletBond<? extends AnyStream, Device> getInletBond() {
+    public InletBond<? extends AnyStream, ? extends Device> getInletBond() {
         return inletBond;
     }
 
-    public OutletBond<? extends AnyStream, Device> getOutletBond() {
+    public OutletBond<? extends AnyStream, ? extends Device> getOutletBond() {
         return outletBond;
     }
 }
--- src/test/java/wromaciej/hvac_sim/thermo/bonds/BondTest.java
+++ src/test/java/wromaciej/hvac_sim/thermo/bonds/BondTest.java
@@ -5,6 +5,7 @@ import wromaciej.hvac_sim.ids.IdGenerator;
 import wromaciej.hvac_sim.ids.Ids;
 import wromaciej.hvac_sim.thermo.devices.model.basic.Device;
 import wromaciej.hvac_sim.thermo.devices.model.basic.Heater;
+import wromaciej.hvac_sim.thermo.devices.model.basic.bonds.OutletBond;
 import wromaciej.hvac_sim.thermo.matter.fluids.model.Fluid;
 import wromaciej.hvac_sim.thermo.streams.model.FluidStream;
 
@@ -16,7 +17,14 @@ public class BondTest {
         IdGenerator idGenerator = new Ids();
         Fluid fluid = new Fluid();
         FluidStream fluidStream = new FluidStream(idGenerator.getUniqueId(), idGenerator, fluid);
-        Device device = new Heater(idGenerator.getUniqueId(), idGenerator);
+        Heater heater = new Heater(idGenerator.getUniqueId(), idGenerator);
+
+        //WHEN
+        OutletBond<FluidStream, Device> outletBond = new OutletBond<>(idGenerator.getUniqueId(), heater);
+        outletBond.connectTo(heater.getFluidInlet()); //IT WORKS
+        outletBond.connectTo(heater.getFluidOutlet()); //IT DOES NOT WORK - GOOD
+
+        fluidStream.getOutletBond().connectTo(heater.getFluidInlet()); //IT DOES NOT WORK - BAD
 
 
     }
