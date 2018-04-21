aa3e0a14f5b8c6ce4898caaf474095413eb3f08b
Maciej Wróblewski <wro.maciej@gmail.com>
2018-04-22 00:07:09 +0200

    Changing Map to List in fluid solver

--- src/main/java/wromaciej/hvac_sim/solver/FluidSolver.java
+++ src/main/java/wromaciej/hvac_sim/solver/FluidSolver.java
@@ -7,13 +7,14 @@ import wromaciej.hvac_sim.thermo.matter.fluids.parameters.Parameter;
 import wromaciej.hvac_sim.thermo.matter.fluids.parameters.ParameterType;
 import wromaciej.hvac_sim.thermo.matter.fluids.service.FluidFactory;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class FluidSolver {
 
-    private class KnownFluidParameters{
+    private class KnownFluidParameters {
         public Parameter knownParameter1;
         public Parameter knownParameter2;
 
@@ -21,7 +22,7 @@ public class FluidSolver {
         }
     }
 
-    private class KnownAirParameters extends KnownFluidParameters{
+    private class KnownAirParameters extends KnownFluidParameters {
         public Parameter airPressure;
 
         public KnownAirParameters() {
@@ -30,13 +31,22 @@ public class FluidSolver {
 
     private FluidName fluidName;
 
-    private Map<ParameterType, Parameter> knownParameters;
+    //private Map<ParameterType, Parameter> knownParameters;
 
     private List<Parameter> allParameters;
 
+    public void setFluidName(FluidName fluidName) {
+        this.fluidName = fluidName;
+    }
+
+    public FluidName getFluidName() {
+        return fluidName;
+    }
+
     public FluidSolver(FluidName fluidName, Parameter... parameters) {
         this.fluidName = fluidName;
-        this.knownParameters = new HashMap<>();
+        //this.knownParameters = new HashMap<>();
+        this.allParameters = new ArrayList<>();
 
         for (Parameter parameter : parameters) {
             addParameter(parameter);
@@ -46,83 +56,109 @@ public class FluidSolver {
     public FluidSolver(Fluid fluid) {
         this(fluid.getFluidName(),
                 fluid.getAbsoluteTemperature(),
-                fluid.getAbsolutePressure(),
-                fluid.getSpecificEnthalpy(),
-                fluid.getSpecificEntropy(),
-                fluid.getQuality(),
-                fluid.getHeatCapacity(),
-                fluid.getSpecificVolume(),
-                fluid.getDensity());
+                //fluid.getAbsolutePressure(),
+                //fluid.getSpecificEnthalpy(),
+                fluid.getSpecificEntropy());
+                //fluid.getQuality(),
+                //fluid.getHeatCapacity(),
+                //fluid.getSpecificVolume(),
+                //fluid.getDensity());
     }
 
     public FluidSolver(Air air) {
         this(air.getFluidName(),
                 air.getAbsoluteTemperature(),
                 air.getAbsolutePressure(),
-                air.getSpecificEnthalpy(),
-                air.getSpecificEntropy(),
-                air.getQuality(),
-                air.getHeatCapacity(),
-                air.getSpecificVolume(),
-                air.getDensity(),
-                air.getDewPointTemperature(),
-                air.getMoistureContent(),
-                air.getRelativeHumidity(),
-                air.getWaterFraction(),
-                air.getWaterPartialPressure(),
-                air.getWetBulbTemperature());
+                //air.getSpecificEnthalpy(),
+                //air.getSpecificEntropy(),
+                //air.getQuality(),
+                //air.getHeatCapacity(),
+                //air.getSpecificVolume(),
+                //air.getDensity(),
+                //air.getDewPointTemperature(),
+                air.getMoistureContent());
+                //air.getRelativeHumidity(),
+                //air.getWaterFraction(),
+                //air.getWaterPartialPressure(),
+                //air.getWetBulbTemperature());
 
     }
 
-    public void clearParameters() {
-        knownParameters.clear();
+    public void clearAllParameters() {
+        //knownParameters.clear();
+        allParameters.clear();
     }
 
     public void clearFluidName() {
         fluidName = null;
     }
 
-    public void clearParameter(ParameterType parameterType) {
-        knownParameters.remove(parameterType);
-    }
+//    public void clearParameter(ParameterType parameterType) {
+//        //knownParameters.remove(parameterType);
+//    }
 
-    public void clearParameter(Parameter parameter) {
-        knownParameters.remove(parameter.getParameterType());
+    public void removeParameter(Parameter parameter) {
+        //knownParameters.remove(parameter.getParameterType());
+        allParameters.remove(parameter);
     }
 
     public void addParameter(Parameter knownParameter) {
-        if (knownParameter.getParameterType() != ParameterType.OTHER)
-            knownParameters.put(knownParameter.getParameterType(), knownParameter);
+        if ((knownParameter.getParameterType() != ParameterType.OTHER)
+                && (allParameters.indexOf(knownParameter) == -1))
+            //knownParameters.put(knownParameter.getParameterType(), knownParameter);
+            allParameters.add(knownParameter);
     }
 
-    public Parameter getParameterByType(ParameterType parameterType) {
-        return knownParameters.get(parameterType);
+    private boolean isOneParameterOfGivenType(ParameterType checkingParameter) {
+        char numberOfParameters = 0;
+        for (Parameter parameter : allParameters) {
+            if (parameter.getParameterType() == checkingParameter)
+                numberOfParameters++;
+            if (numberOfParameters > 1) break;
+        }
+        if (numberOfParameters == 1) return true;
+        else return false;
     }
+
+    private int numberOfDifferentParameters(Fluid fluid){
+
+    }
+
+
+//    public Parameter getParameterByType(ParameterType parameterType) {
+//        //return knownParameters.get(parameterType);
+//    }
 
 
     private int numberOfKnownParameters() {
-        return knownParameters.size();
+        //return knownParameters.size();
+        return allParameters.size();
     }
 
 
-    public SolverResult isSolvable(Fluid fluid) {
-
+    public SolverResult solverResult(Fluid fluid) {
         if ((fluidName != null) && (fluid.fluidSolver.numberOfKnownParameters() == 2)) return SolverResult.GOOD_DATA
         else if (fluid.fluidSolver.numberOfKnownParameters() < 2) return SolverResult.INSUFFICIENT_DATA;
         else return SolverResult.TOO_MANY_CONSTRAINTS;
     }
 
-    public SolverResult isSolvable(Air air){
+    public boolean isSolvable(Fluid fluid) {
+        if (fluid.fluidSolver.solverResult(fluid) == SolverResult.GOOD_DATA) return true;
+        else return false;
+    }
+
+    public SolverResult isSolvable(Air air) {
 
 
     }
 
-    private KnownFluidParameters knownFluidParameters(){
+    private KnownFluidParameters knownFluidParameters() {
         KnownFluidParameters knownFluidParameters = new KnownFluidParameters();
         knownFluidParameters.knownParameter1 = knownParameters.
     }
 1
-    private KnownAirParameters knownAirParameters(){
+
+    private KnownAirParameters knownAirParameters() {
 //        KnownAirParameters knownAirParameters = new KnownAirParameters();
 //        knownAirParameters.knownParameter1 = null;
 //        kn
@@ -132,36 +168,9 @@ public class FluidSolver {
 
     public Fluid solvedFluid(Fluid fluid, FluidFactory fluidFactory) {
         if (isSolvable(fluid)) {
-            fluid = fluidFactory.createFluid(fluid.fluidSolver.fluidName,  );
-        }
-        else return null;
+            fluid = fluidFactory.createFluid(fluid.fluidSolver.fluidName, );
+        } else return null;
     }
 
-    //    public int notSolvedItemsNumber() {
-//        int notSolvedSum = 0;
-//        for (Parameter parameter : parameters) {
-//            if (parameter.isSolved() == false) notSolvedSum++;
-//        }
-//        return notSolvedSum;
-//    }
-//
-//    public Set<Parameter> solvedItems() {
-//        Set<Item> solvedItemsSet = new HashSet<>();
-//        for (Parameter parameter : parameters) {
-//            if (parameter.isSolved()) solvedItemsSet.add(parameter);
-//        }
-//        return solvedItemsSet;
-//    }
-//
-//    public Set<Parameter> notSolvedItems() {
-//        Set<Parameter> notSolvedItemsSet = new HashSet<>();
-//        for (Parameter parameter : parameters) {
-//            if (!parameter.isSolved()) notSolvedItemsSet.add(parameter);
-//        }
-//        return notSolvedItemsSet;
-//    }
-//
-//    public boolean isEverythingSolved(){
-//        return notSolvedItems().isEmpty();
-//    }
+
 }
