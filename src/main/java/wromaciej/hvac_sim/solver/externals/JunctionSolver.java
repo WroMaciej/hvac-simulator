9f9543da9cd2023605fd6c7a7f0db6c8e0bd9699
Maciej Wróblewski <wro.maciej@gmail.com>
2018-05-02 12:36:36 +0200

    Junction and JunctionSolver done but not tested

--- src/main/java/wromaciej/hvac_sim/solver/externals/JunctionSolver.java
+++ src/main/java/wromaciej/hvac_sim/solver/externals/JunctionSolver.java
@@ -3,68 +3,76 @@ package wromaciej.hvac_sim.solver.externals;
 import wromaciej.hvac_sim.solver.result.SolverResult;
 import wromaciej.hvac_sim.solver.result.SolverResultType;
 import wromaciej.hvac_sim.thermo.generals.bonds.BondDirection;
+import wromaciej.hvac_sim.thermo.generals.bonds.Junction;
 import wromaciej.hvac_sim.thermo.generals.bonds.ParameterWithDirection;
 import wromaciej.hvac_sim.thermo.matter.fluids.parameters.Parameter;
 
+import java.util.ArrayList;
 import java.util.List;
 
-/**
- *
- */
 
-public class JunctionSolver {
+public class JunctionSolver implements ExternalSolver<Junction> {
 
 
-    private List<ParameterWithDirection> allParameters;
-    private List<ParameterWithDirection> undefinedParameters;
-    private List<ParameterWithDirection> definedParameters;
+    public List<ParameterWithDirection> getAllParameters(Junction toSolve) {
+        return toSolve.getAllParameters();
+    }
 
-
-    public JunctionSolver(List<ParameterWithDirection> parametersWithDirections) {
-        if (parametersWithDirections != null){
-            for (ParameterWithDirection parameterWithDirection : parametersWithDirections) {
-                allParameters.add(parameterWithDirection);
-                if (parameterWithDirection.getParameter().isDefined()) definedParameters.add(parameterWithDirection);
-                else undefinedParameters.add(parameterWithDirection);
-            }
+    public List<ParameterWithDirection> getUndefinedParameters(Junction toSolve) {
+        List<ParameterWithDirection> undefinedParameters = new ArrayList<>();
+        for (ParameterWithDirection parameterWithDirection : getAllParameters(toSolve)) {
+            if (!parameterWithDirection.getParameter().isDefined()) undefinedParameters.add(parameterWithDirection);
         }
+        return undefinedParameters;
     }
 
-    private int allParametersNumber(){
-        return allParameters.size();
+    public List<ParameterWithDirection> getDefinedParameters(Junction toSolve) {
+        List<ParameterWithDirection> definedParameters = new ArrayList<>();
+        for (ParameterWithDirection parameterWithDirection : getAllParameters(toSolve)) {
+            if (parameterWithDirection.getParameter().isDefined()) definedParameters.add(parameterWithDirection);
+        }
+        return definedParameters;
     }
 
-    private int definedParametersNumber(){
-        return definedParameters.size();
-    }
-    private int undefinedParametersNumber(){
-        return undefinedParameters.size();
+    public JunctionSolver() {}
+
+    private int allParametersNumber(Junction toSolve){
+        return getAllParameters(toSolve).size();
     }
 
-    private Parameter getUndefinedParameter(){
-        if (undefinedParametersNumber() == 1) return undefinedParameters.get(0);
-        else return null;
+    private int definedParametersNumber(Junction toSolve){
+        return getDefinedParameters(toSolve).size();
+    }
+    private int undefinedParametersNumber(Junction toSolve){
+        return getUndefinedParameters(toSolve).size();
     }
 
-    private void setUndefinedParameter(Parameter parameter){
-        getUndefinedParameter()=null;
-    }
 
-    private ParameterWithDirection getBalance(){
+    private ParameterWithDirection getBalance(Junction toSolve){
         Parameter sum = new Parameter();
-        for (ParameterWithDirection parameterWithDirection : definedParameters){
+        ParameterWithDirection resultingParameterWithDirection;
+        for (ParameterWithDirection parameterWithDirection : getDefinedParameters(toSolve)){
             sum = sum.plus(parameterWithDirection.getParameter().times(parameterWithDirection.getDirection().balanceMultiplyer));
         }
-
+        if (sum.isPositive()) resultingParameterWithDirection = new ParameterWithDirection(sum.abs(), BondDirection.OUTLET);
+        else resultingParameterWithDirection = new ParameterWithDirection(sum.abs(), BondDirection.INLET);
+        return resultingParameterWithDirection;
     }
 
 
 
-    public SolverResult solve{
+    @Override
+    public SolverResult solve(Junction toSolve) {
         SolverResultType solverResultType;
-        if (undefinedParametersNumber() == 1){
-            getUndefinedParameter()=null;
-        }
+        List<ParameterWithDirection> undefinedParameters = getUndefinedParameters(toSolve);
 
+        if (undefinedParametersNumber(toSolve)>1) solverResultType = SolverResultType.NOT_SOLVED_NODATA;
+        else if (undefinedParametersNumber(toSolve)<1) solverResultType = SolverResultType.NOT_SOLVED_TOO_MUCH_DATA;
+        else{
+            undefinedParameters.get(0).setParameter(getBalance(toSolve).getParameter());
+            undefinedParameters.get(0).setDirection(getBalance(toSolve).getDirection());
+            solverResultType = SolverResultType.SOLVED;
+        }
+        return new SolverResult(null, solverResultType);
     }
 }
--- src/main/java/wromaciej/hvac_sim/thermo/generals/bonds/Junction.java
+++ src/main/java/wromaciej/hvac_sim/thermo/generals/bonds/Junction.java
@@ -1,4 +1,31 @@
 package wromaciej.hvac_sim.thermo.generals.bonds;
 
-public class Junction {
+import wromaciej.hvac_sim.solver.externals.JunctionSolver;
+import wromaciej.hvac_sim.solver.internals.Solvable;
+import wromaciej.hvac_sim.solver.result.SolverResult;
+
+import java.util.List;
+
+public class Junction implements Solvable{
+    private JunctionSolver junctionSolver;
+
+    private List<ParameterWithDirection> allParameters;
+
+    public List<ParameterWithDirection> getAllParameters() {
+        return allParameters;
+    }
+
+    public Junction(List<ParameterWithDirection> allParameters, JunctionSolver junctionSolver) {
+        this.allParameters = allParameters;
+    }
+
+    @Override
+    public SolverResult solve() {
+        return junctionSolver.solve(this);
+    }
+
+    @Override
+    public boolean isSolved() {
+        return false;
+    }
 }
