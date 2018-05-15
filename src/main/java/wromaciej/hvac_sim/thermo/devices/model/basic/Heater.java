f94f38850636554ddf8a5ce2e051f7b172f655ef
Maciej Wróblewski <wro.maciej@gmail.com>
2018-05-15 09:26:01 +0200

    beginning of heater

--- src/main/java/wromaciej/hvac_sim/thermo/devices/model/basic/Heater.java
+++ src/main/java/wromaciej/hvac_sim/thermo/devices/model/basic/Heater.java
@@ -1,20 +1,26 @@
 package wromaciej.hvac_sim.thermo.devices.model.basic;
 
 import wromaciej.hvac_sim.ids.IdGenerator;
+import wromaciej.hvac_sim.thermo.generals.bonds.BondDirection;
+import wromaciej.hvac_sim.thermo.generals.bonds.OutletDeviceBond;
 import wromaciej.hvac_sim.thermo.generals.conservationLaw.Channel;
 import wromaciej.hvac_sim.thermo.generals.bonds.InletDeviceBond;
+import wromaciej.hvac_sim.thermo.matter.fluids.parameters.Parameter;
+import wromaciej.hvac_sim.thermo.quantities.specific.PressureDifference;
 import wromaciej.hvac_sim.thermo.streams.model.HeatStream;
 import wromaciej.hvac_sim.thermo.streams.model.MatterStream;
 
 public class Heater extends Device {
     public final InletDeviceBond<HeatStream> heatInlet;
 
-    private final Channel<MatterStream> channel;
+    private final Channel<MatterStream> mainChannel;
 
-    public Heater(int id, IdGenerator idGenerator, InletDeviceBond<HeatStream> heatInlet, Channel<MatterStream> channel) {
+    public Heater(int id, IdGenerator idGenerator, Channel<MatterStream> mainChannel, InletDeviceBond<HeatStream> heatInlet) {
         super(id, idGenerator);
+        this.mainChannel = mainChannel;
         this.heatInlet = heatInlet;
-        this.channel = channel;
+        mainChannel.getHeatFlow().setDirection(BondDirection.INLET);
+        mainChannel.getHeatFlow().setParameter(heatInlet.getTargetBond().getOwnerItem().getHeatFlow());
     }
 
 
