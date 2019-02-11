e7a52aefc8165940cd856b0bfa004bd66bbe866c
WroMaciej <maciej.a.wroblewski@capgemini.com>
2019-02-11 12:15:57 +0100

    Reconfugure DB to MySQL

--- src/main/java/wromaciej/hvac_sim/core/database/repository/core/CompanyRepository.java
+++ src/main/java/wromaciej/hvac_sim/core/database/repository/core/CompanyRepository.java
@@ -1,12 +1,13 @@
 package wromaciej.hvac_sim.core.database.repository.core;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import wromaciej.hvac_sim.core.model.Company;
 
 
 @Repository
-public interface CompanyRepository extends CrudRepository<Company, Integer> {
+public interface CompanyRepository extends JpaRepository<Company, Integer> {
 
 
 
--- src/test/java/wromaciej/hvac_sim/simulation/thermo/fluids/IdealProcessTest.java
+++ src/test/java/wromaciej/hvac_sim/simulation/thermo/fluids/IdealProcessTest.java
@@ -1,41 +1,50 @@
-package wromaciej.hvac_sim.thermo.fluids;
+package wromaciej.hvac_sim.simulation.thermo.fluids;
 
-import org.junit.Test;
-import wromaciej.hvac_sim.thermo.matter.fluids.model.Fluid;
-import wromaciej.hvac_sim.thermo.matter.fluids.parameters.FluidName;
-import wromaciej.hvac_sim.thermo.matter.fluids.parameters.Parameter;
-import wromaciej.hvac_sim.thermo.matter.fluids.parameters.ParameterType;
-import wromaciej.hvac_sim.thermo.matter.fluids.service.FluidData;
-import wromaciej.hvac_sim.thermo.matter.fluids.service.FluidFactory;
-import wromaciej.hvac_sim.thermo.matter.fluids.service.IdealProcess;
-import wromaciej.hvac_sim.thermo.quantities.specific.Pressure;
-import wromaciej.hvac_sim.thermo.quantities.specific.SpecificEnthalpy;
-import wromaciej.hvac_sim.thermo.quantities.specific.Temperature;
-import wromaciej.hvac_sim.thermo.quantities.specific.TemperatureDifference;
+import static org.junit.Assert.assertEquals;
 
 import javax.measure.unit.NonSI;
 import javax.measure.unit.SI;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import wromaciej.hvac_sim.simulation.thermo.matter.fluids.model.Fluid;
+import wromaciej.hvac_sim.simulation.thermo.matter.fluids.model.FluidFactory;
+import wromaciej.hvac_sim.simulation.thermo.matter.fluids.model.FluidName;
+import wromaciej.hvac_sim.simulation.thermo.matter.fluids.service.processes.IdealProcess;
+import wromaciej.hvac_sim.simulation.thermo.parameters.Parameter;
+import wromaciej.hvac_sim.simulation.thermo.parameters.ParameterType;
+import wromaciej.hvac_sim.simulation.thermo.quantities.specific.Pressure;
+import wromaciej.hvac_sim.simulation.thermo.quantities.specific.SpecificEnthalpy;
+import wromaciej.hvac_sim.simulation.thermo.quantities.specific.Temperature;
+import wromaciej.hvac_sim.simulation.thermo.quantities.specific.TemperatureDifference;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class IdealProcessTest {
 
-
+	
+	FluidFactory fluidFactory;
+	
+	@Autowired
+	IdealProcess idealProcess;
 
     @Test
     public void shouldReturnSameTemperatureDifferenceByIsobaricHeatingOfWaterAndEquationCalculation(){
         //GIVEN
-        FluidData fluidData = new FluidData();
-        fluidData.loadLibrary();
-        FluidFactory fluidFactory= new FluidFactory(fluidData);
-        IdealProcess idealProcess = new IdealProcess(fluidFactory);
+//        FluidData fluidData = new FluidData(true);
+//        FluidSolver fluidSolver = new FluidSolver(fluidSetter);
+//        IdealProcess idealProcess = new IdealProcess(fluidFactory);
 
         Parameter<Temperature> temperatureBeforeIsobaricProcess = new Parameter<>(ParameterType.TEMPERATURE, SI.CELSIUS.asType(Temperature.class), 20);
         Parameter<Temperature> temperatureAfterIsobaricProcess = new Parameter<>(ParameterType.TEMPERATURE, SI.CELSIUS.asType(Temperature.class), 50);
         Parameter<Pressure> pressure = new Parameter<>(ParameterType.PRESSURE, NonSI.BAR.asType(Pressure.class), 1.0);
         Fluid fluidBeforeProcess = fluidFactory.createFluid(FluidName.WATER, temperatureBeforeIsobaricProcess, pressure);
         //WHEN
-        Fluid fluidAfterProcess = idealProcess.isobaric(fluidBeforeProcess, temperatureAfterIsobaricProcess);
+        Fluid fluidAfterProcess = idealProcess.samePressure(fluidBeforeProcess, temperatureAfterIsobaricProcess);
         Parameter<TemperatureDifference> deltaTempDuringProcess= fluidAfterProcess.getTemperature().minus(fluidBeforeProcess.getTemperature());
         Parameter<SpecificEnthalpy> deltaEnthalpyDuringProcess = fluidAfterProcess.getSpecificEnthalpy().minus(fluidBeforeProcess.getSpecificEnthalpy());
         Parameter<Temperature> deltaTempDuringEnthalpyCalculation = deltaEnthalpyDuringProcess.divide(fluidAfterProcess.getHeatCapacity());
